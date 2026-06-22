3cc3ad93b8cb8bd21b2787ef25d402aca3e161b0
AntonTower <Anton.tower21@gmail.com>
2026-06-21 21:43:45 -0600

    feat: se actualiza la API URL a producción en Railway

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
+  /*    ENLACE LOCAL
   apiUrl: 'http://localhost:3000/api'
+  */
+ // ENLACE GLOBAL
+  apiUrl:'https://rescuenet-backend-production.up.railway.app/'
 };
