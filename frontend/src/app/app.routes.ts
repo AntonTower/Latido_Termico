125a7b397c2ca73d43ce55b12bba232d5e1ade06
Chris5Flov <c.tend145@gmail.com>
2026-07-16 20:05:59 -0600

    Inicio a los enlaces del patrocinador

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from '@angular/router';
-// 👇 1. Agregamos "Component" al final de los nombres
 import { MisReportesComponent } from './pages/cliente-basico/mis-reportes/mis-reportes';
 import { MisCasosComponent } from './pages/cliente-basico/mis-casos/mis-casos';
 
@@ -23,15 +22,13 @@ export const routes: Routes = [
   // 🛡️ 2. ENTORNO PRIVADO EXCLUSIVO (Miembros Registrados)
   {
     path: 'operaciones',
-    // 👇 Nombres cortos aquí
     loadComponent: () => import('./layouts/member-layout/member-layout').then(m => m.MemberLayoutComponent),
     children: [
       { path: '', loadComponent: () => import('./pages/cliente-basico/dashboard/dashboard').then(m => m.DashboardComponent)},
       { path: 'mis-datos', loadComponent: () => import('./pages/cliente-basico/mis-datos/mis-datos').then(m => m.MisDatosComponent)},
-      
-      // 👇 2. Actualizamos aquí también
-      { path: 'mis-reportes', component: MisReportesComponent }, // 🌟 CORREGIDO
-      { path: 'mis-casos', component: MisCasosComponent }        // 🌟 CORREGIDO
+      { path: 'mis-reportes', component: MisReportesComponent },
+      { path: 'mis-casos', component: MisCasosComponent },
+      { path: 'patrocinador', loadComponent: () => import('./pages/patrocinador/patrocinador').then(m => m.PatrocinadorComponent) }
     ]
   },
 
