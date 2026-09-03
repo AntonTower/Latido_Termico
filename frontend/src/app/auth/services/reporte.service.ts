5cdd4c88bd93b37f946ed871eb6db339bb186c87
AntonTower <Anton.tower21@gmail.com>
2026-09-03 09:40:55 -0600

    Chris New Versión Vercel

--- frontend/src/app/auth/services/reporte.service.ts
+++ frontend/src/app/auth/services/reporte.service.ts
@@ -24,6 +24,7 @@ export class ReporteService {
     return this.http.get<any[]>(`${this.apiUrl}/mis-reportes`, { headers: this.getAuthHeaders() });
   }
 
+
   getMisRescatesTomados(): Observable<any[]> {
     return this.http.get<any[]>(`${this.apiUrl}/mis-reportes`, { headers: this.getAuthHeaders() }); 
   }
